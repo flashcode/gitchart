a6bd164fc5e588bff37a3668858fa745e9c67f3b
Thomas Grainger <tom@procensus.com>
2017-03-22 12:30:06 +0000

    drop support for Python 2.6

--- setup.py
+++ setup.py
@@ -29,7 +29,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': ['gitchart=gitchart:main'],
