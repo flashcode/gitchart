d119696cf3eb27de532943ca9e7e75ca632efc63
Sébastien Helleu <flashcode@flashtux.org>
2018-12-01 08:07:01 +0100

    Use https for links to GNU GPL license

--- gitchart.py
+++ gitchart.py
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with gitchart.  If not, see <http://www.gnu.org/licenses/>.
+# along with gitchart.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 Generate statistic charts on Git repositories using pygal (http://pygal.org).
