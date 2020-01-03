f0bfdc4cc72805c9dd50ea06dc7973ec922a0d09
Sébastien Helleu <flashcode@flashtux.org>
2020-01-03 23:45:42 +0100

    Update copyright dates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2013-2019 Sébastien Helleu <flashcode@flashtux.org>
+# Copyright (C) 2013-2020 Sébastien Helleu <flashcode@flashtux.org>
 #
 # This file is part of gitchart.
 #
